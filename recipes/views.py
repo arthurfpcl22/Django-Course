76f73ea69ccb4f8b03e0dc58c062cb16af1d5427
Arthur Franca <arthurfpcl2@gmail.com>
2022-06-19 17:48:09 -0300

    atual

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,19 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
+
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Arthur Franca'
+        "recipes": [make_recipe() for _ in range(10)]
     })
 
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipes-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
 
 # Create your views here.
