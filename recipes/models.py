c1384fee145178568160b451c11ceb63e7f5cbd9
Arthur Franca <arthurfpcl2@gmail.com>
2022-06-26 12:42:21 -0300

    atual

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
 
 
 # Create your models here.
@@ -16,3 +24,9 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
